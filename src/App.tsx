af65160229b1171b6d6fd2b5fdd0bfb222a6476c
DhananjayPatilkaki <dhananjay@patilkaki.com>
2023-12-29 23:05:39 +0530

    added postzone and tagTypes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 
 
 import './App.css'
-import { useGetZonesQuery } from './store/apicall'
+import { Zones } from './modal/props';
+import { useGetZonesQuery, usePostZonesMutation } from './store/apicall'
 
 function App() {
 	const { isLoading, error, data } = useGetZonesQuery("");
+	const [newZones] = usePostZonesMutation();
 
-	console.log(isLoading, error, data)
+	const handleSubmit = () => {
+
+		const zone: Zones = {
+			label: "",
+			filterType: "",
+			filterValue: ""
+		}
+		newZones(zone)
+	}
 	return (
 		<>
 			<div>
 				{isLoading && <p>...loading</p>}
-	
+
 				{data?.map((item) => {
 					return (
 						<p>{item.label}</p>
 					)
 				})}
 
-
+				<form onSubmit={handleSubmit} style={{ marginTop: "2rem" }}>
+					<div  style={{marginTop:"1rem"}} >
+						<label>Enter Label Name :</label>
+						<input type="text" placeholder='Label' />
+					</div>
+					<div style={{ marginTop: "1rem" }}>
+						<label>Filter Type :</label>
+						<input type="text" placeholder='Filter Type' />
+					</div>
+					<div style={{ marginTop: "1rem" }}>
+						<label>Filter Value : </label>
+						<input type="text" placeholder='Filter Value' />
+					</div>
+					<button type='submit' style={{ marginTop: "1rem" }}>Submit</button>
+				</form>
 				{/* {error && <p> {error} </p>} */}
 
 			</div>
--- src/store/apicall.tsx
+++ src/store/apicall.tsx
@@ -3,14 +3,25 @@ import { Zones } from "../modal/props";
 
 
 export const zonesApi = createApi({
-	reducerPath:'zonesApi',
-	baseQuery: fetchBaseQuery({ baseUrl: 'https://flexy-aws.coyogi.com/ms/groups'}),
-	endpoints:(builder) => ({
+	reducerPath: 'zonesApi',
+	baseQuery: fetchBaseQuery({ baseUrl: 'https://flexy-aws.coyogi.com/ms/groups' }),
+	tagTypes:['Zones'],
+	endpoints: (builder) => ({
 		getZones: builder.query<Zones[], string>({
 			query: () => `/connect/filters`,
-	 })
+			providesTags:['Zones']
+		}),
+		postZones: builder.mutation<Zones, Zones>({
+			query: (post) => ({
+				url: '/connect/createFilter',
+				method: 'POST',
+				body: post
+			}),
+			invalidatesTags: ["Zones"]
+		})
+
 	})
 })
 
 
-export const  { useGetZonesQuery} = zonesApi
+export const { useGetZonesQuery , usePostZonesMutation } = zonesApi
